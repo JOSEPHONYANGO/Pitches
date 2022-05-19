f0eb46417b01a780bee3c347a8735525fb9b671c
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-05-19 21:03:19 +0300

    aws

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from app import create_app,db
-from flask_script import Manager,Server
-from app.models import User
-from flask_migrate import Migrate, MigrateCommand
+# from flask import Flask
+from app import create_app
+# from flask_script import Manager,Server
+# from app.models import User
+# from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 # app = create_app('production')
